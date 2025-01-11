9ac5b8659002b9a8ce21dd8eff768d1ecd7e2c99
Dawid Musialik <dawidmusialik1998@gmail.com>
2025-01-11 21:14:23 +0100

    separated backend and frontend to other projects, done initial server stuff

---
+++ MathGameServer/Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+var hostName = Dns.GetHostName();
+
+Console.WriteLine(hostName);
+
+var hostEntry = Dns.GetHostEntry(hostName);
+foreach(var ip in hostEntry.AddressList)
+    Console.WriteLine(ip);
+
+IPAddress ipAddress = hostEntry.AddressList[0];
+IPEndPoint ipEndPoint = new(ipAddress, 8080);
+
+using Socket listener = new(
+        ipEndPoint.AddressFamily,
+        SocketType.Stream,
+        ProtocolType.Tcp);
+
+listener.Bind(ipEndPoint);
+listener.Listen(5);
+
+var handler = await listener.AcceptAsync();
+
+while (true)
+{
+    var buffer = new byte[1024];
+    var receivedBytes = await handler.ReceiveAsync(buffer, SocketFlags.None);
+    var message = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+
+    Console.WriteLine($"Received: {message}");
+
+    const string eom = "<|EOM|>";
+    const string ack = "<|ACK|>";
+
+    var eomWasSend = message.IndexOf(eom) > -1;
+    if (eomWasSend)
+    {
+        Console.WriteLine($"Server received message: {message}");
+        
+        var echoBytes = Encoding.UTF8.GetBytes(ack);
+        await handler.SendAsync(echoBytes, 0);
+        Console.WriteLine("Server send acknowledge messge");
+
+        break;
+    }
+}
